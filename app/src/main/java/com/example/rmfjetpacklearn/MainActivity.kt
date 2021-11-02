8d00d6ad25aa6551095b8d5ed39c9b5b39cb9282
RMF <reksamfitrananda@gmail.com>
2021-11-02 09:22:00 +0700

    State

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -3,117 +3,73 @@ package com.example.rmfjetpacklearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            StylingText()
+            Column(Modifier.fillMaxSize()) {
+
+                val color = remember {
+                    mutableStateOf(Color.Blue)
+                }
+
+                State(
+                    Modifier
+                        .fillMaxSize()
+                        .weight(1f)
+                ) {
+                    color.value = it
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
         }
     }
 }
 
 @Composable
-fun StylingText() {
-    val fontFamily = FontFamily(
-        Font(R.font.lexend_thin, FontWeight.Thin),
-        Font(R.font.lexend_light, FontWeight.Light),
-        Font(R.font.lexend_regular, FontWeight.Normal),
-        Font(R.font.lexend_medium, FontWeight.Medium),
-        Font(R.font.lexend_semi_vold, FontWeight.SemiBold),
-        Font(R.font.lexend_bold, FontWeight.Bold),
-        Font(R.font.lexend_extra_bold, FontWeight.ExtraBold)
-    )
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color(0xFF101010))
-    ) {
-        Column {
-            Text(
-                text = "Jetpack Compose",
-                color = Color.White,
-                fontSize = 30.sp,
-                fontFamily = fontFamily,
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Italic,
-                textAlign = TextAlign.Center
-            )
+fun State(modifier: Modifier = Modifier, updatedColor: (Color) -> Unit) {
 
-            Text(
-                text = buildAnnotatedString {
-                    withStyle(
-                        style = SpanStyle(
-                            color = Color.Magenta,
-                            fontSize = 40.sp
-                        )
-                    ) {
-                        append("J")
-                    }
-                    append("etpack ")
-
-                    withStyle(
-                        style = SpanStyle(
-                            color = Color.Magenta,
-                            fontSize = 40.sp
-                        )
-                    ) {
-                        append("C")
-                    }
-                    append("ompose")
-                },
-                color = Color.White,
-                fontSize = 30.sp,
-                fontFamily = fontFamily,
-                fontWeight = FontWeight.Bold,
-                fontStyle = FontStyle.Italic,
-                textAlign = TextAlign.Center
+    Box(modifier = modifier
+        .background(Color.Green)
+        .clickable {
+            updatedColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat()
+                )
             )
-        }
+        }) {
+
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-
-    Text(text = "Hello")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RMFJetpackLearnTheme {
-        StylingText()
+
     }
 }
