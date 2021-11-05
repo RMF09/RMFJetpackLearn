2ee7fae0f32d9715300005895cd80c22daefae24
RMF <reksamfitrananda@gmail.com>
2021-11-05 16:29:39 +0700

    list item

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -14,8 +18,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
 import kotlinx.coroutines.launch
 
@@ -24,68 +30,57 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            TextSnackBtn()
+            ListItem()
         }
     }
 }
 
 @Composable
-fun TextSnackBtn() {
-    val scaffoldState = rememberScaffoldState()
-    var textFieldState by remember {
-        mutableStateOf("")
-    }
-    val scope = rememberCoroutineScope()
-
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        scaffoldState = scaffoldState
-    ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 30.dp)
-        ) {
-            TextField(
-                value = textFieldState,
-                label = {
-                    Text(text = "Enter your name")
-                },
-                onValueChange = {
-                    textFieldState = it
-                },
-                singleLine = true,
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Search,
-                    keyboardType = KeyboardType.Text
-                ),
-                keyboardActions = KeyboardActions(
-                    onSearch = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                        }
-                    }
-                )
+fun ListItem() {
+    val scrollState = rememberScrollState()
+    /*Column(modifier = Modifier.verticalScroll(scrollState)) {
+        for (i in 1..50) {
+            Text(
+                text = "item $i",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
             )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = {
-                scope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                }
-            }) {
-                Text(text = "please greet me")
-            }
         }
+    }*/
+    LazyColumn {
+        itemsIndexed(
+            listOf("This","is","Jetpack", "Compose")
+        ){ index, item ->
+            Text(
+                text = "item $index : $item",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }
+        /*items(7000){
+            Text(
+                text = "item $it",
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 24.dp)
+            )
+        }*/
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RMFJetpackLearnTheme {
-        TextSnackBtn()
+        ListItem()
     }
 }
