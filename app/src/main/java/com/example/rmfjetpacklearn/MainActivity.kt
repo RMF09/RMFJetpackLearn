ecd611b8c285cab2ce6d5302bebf71874b1624b3
RMF <reksamfitrananda@gmail.com>
2021-11-17 09:57:09 +0700

    constrain layout

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rmfjetpacklearn
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -16,12 +17,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
 import kotlinx.coroutines.launch
 
@@ -30,49 +36,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ListItem()
+            CL()
         }
     }
 }
 
 @Composable
-fun ListItem() {
-    val scrollState = rememberScrollState()
-    /*Column(modifier = Modifier.verticalScroll(scrollState)) {
-        for (i in 1..50) {
-            Text(
-                text = "item $i",
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-            )
+fun CL() {
+    val constraints = ConstraintSet {
+        val greenBox = createRefFor("greenBox")
+        val redBox = createRefFor("redBox")
+
+        constrain(greenBox) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
         }
-    }*/
-    LazyColumn {
-        itemsIndexed(
-            listOf("This","is","Jetpack", "Compose")
-        ){ index, item ->
-            Text(
-                text = "item $index : $item",
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-            )
+
+        constrain(redBox) {
+            top.linkTo(greenBox.bottom)
+            end.linkTo(parent.end)
+            width = Dimension.value(80.dp)
+            height = Dimension.value(80.dp)
         }
-        /*items(7000){
-            Text(
-                text = "item $it",
-                fontSize = 24.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-            )
-        }*/
+    }
+
+    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+        Box(modifier = Modifier
+            .background(Color.Green)
+            .layoutId("greenBox"))
+
+        Box(modifier = Modifier
+            .background(Color.Red)
+            .layoutId("redBox"))
     }
 }
 
@@ -81,6 +78,6 @@ fun ListItem() {
 @Composable
 fun DefaultPreview() {
     RMFJetpackLearnTheme {
-        ListItem()
+        CL()
     }
 }
