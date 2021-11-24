436f30df35c36df52cd96ccc029f2222b2c45f9c
RMF <reksamfitrananda@gmail.com>
2021-11-24 17:33:00 +0700

    circular progress bar

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -1,106 +1,95 @@
 package com.example.rmfjetpacklearn
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SimpleAnimation()
+            Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+                CircularProgressBar(percentage = 0.8f, number =100)
+            }
         }
     }
 }
 
 @Composable
-fun SimpleAnimation() {
-    var sizeState by remember { mutableStateOf(200.dp) }
-
-    val size by animateDpAsState(
-        targetValue = sizeState,
-        tween(
-            durationMillis = 700,
-            easing = LinearEasing
-        )
-        /*spring(
-            Spring.DampingRatioHighBouncy
-        )*/
-        /*keyframes {
-            durationMillis = 5000
-            sizeState at 0 with LinearEasing
-            sizeState * 1.5f  at 1000 with FastOutSlowInEasing
-            sizeState * 2f at 5000
-
-        }*/
-    )
-
-    val infiniteTransition = rememberInfiniteTransition()
-    val color by infiniteTransition.animateColor(
-        initialValue = Color.Red,
-        targetValue = Color.Green,
-        animationSpec = infiniteRepeatable(
-            tween(
-                durationMillis = 2000,
-                delayMillis = 300,
-                easing = FastOutSlowInEasing
-            ),
-            repeatMode = RepeatMode.Reverse
+fun CircularProgressBar(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 80.dp,
+    color: Color = Color.Red,
+    strokeWidth: Dp = 6.dp,
+    animDuration: Int = 5000,
+    animDelay: Int = 1000
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
         )
     )
 
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
 
     Box(
-        modifier = Modifier
-            .size(size)
-            .background(color),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
     ) {
 
-        Button(onClick = {
-            sizeState += 50.dp
-        }) {
-            Text("Increase Size")
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * curPercentage.value,
+                useCenter = false,
+                style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
         }
+        Text(
+            text = (curPercentage.value * number).toInt().toString() +"%",
+            color = Color.Red,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RMFJetpackLearnTheme {
-        SimpleAnimation()
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            CircularProgressBar(percentage = 0.8f, number =100)
+
+        }
     }
 }
