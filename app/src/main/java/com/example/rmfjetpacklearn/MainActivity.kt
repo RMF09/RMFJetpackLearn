ca66f2163519e5bdcb5f4f0f64cb8eddde35f37d
RMF <reksamfitrananda@gmail.com>
2022-01-17 09:29:23 +0700

    clean import

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -4,32 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +48,7 @@ fun CL() {
         }
     }
 
-    ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+    ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
         Box(modifier = Modifier
             .background(Color.Green)
             .layoutId("greenBox"))
