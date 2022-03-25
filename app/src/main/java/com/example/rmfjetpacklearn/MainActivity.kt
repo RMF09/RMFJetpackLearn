3bdc88085abfb51cac88124e616d6d4f003eb5eb
RMF <reksamfitrananda@gmail.com>
2022-03-25 10:51:55 +0700

    Parallax

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -1,32 +1,33 @@
 package com.example.rmfjetpacklearn
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import com.example.rmfjetpacklearn.ui.theme.BottomNavItem
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,135 +35,113 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            RMFJetpackLearnTheme {
+                val moonScrollSpeed = 0.08f
+                val midBgScrollSpeed = 0.03f
 
-            val navController = rememberNavController()
-            Scaffold(
-                bottomBar = {
-                    BottomNavigationBar(
-                        items = listOf(
-                            BottomNavItem(
-                                name = "Home",
-                                route = "home",
-                                icon = Icons.Default.Home
-                            ),
-                            BottomNavItem(
-                                name = "Chat",
-                                route = "chat",
-                                icon = Icons.Default.Notifications,
-                                badgeCount = 1
-                            ),
-                            BottomNavItem(
-                                name = "Settings",
-                                route = "settings",
-                                icon = Icons.Default.Settings
-                            )
-                        ),
-                        navController = navController,
-                        onItemClick = {
-                            navController.navigate(it.route)
-                        }
-                    )
+                val imageHeight = (LocalConfiguration.current.screenWidthDp * (2f / 3f)).dp
+                val lazyListState = rememberLazyListState()
+
+                var moonOffset by remember {
+                    mutableStateOf(0f)
                 }
-            ) {
-                Navigation(navController = navController)
+                var midBgOffset by remember {
+                    mutableStateOf(0f)
+                }
+
+                val nestedScrollConnection = object : NestedScrollConnection {
+                    override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                        val delta = available.y
+                        val layoutInfo = lazyListState.layoutInfo
+                        // Check if the first item is visible
+                        if(lazyListState.firstVisibleItemIndex == 0) {
+                            return Offset.Zero
+                        }
+                        if(layoutInfo.visibleItemsInfo.lastOrNull()?.index == layoutInfo.totalItemsCount - 1) {
+                            return Offset.Zero
+                        }
+                        moonOffset += delta * moonScrollSpeed
+                        midBgOffset += delta * midBgScrollSpeed
+                        return Offset.Zero
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .nestedScroll(nestedScrollConnection),
+                    state = lazyListState
+                ) {
+                    items(10) {
+                        Text(
+                            text = "Sample item",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .clipToBounds()
+                                .fillMaxWidth()
+                                .height(imageHeight + midBgOffset.toDp())
+                                .background(
+                                    Brush.verticalGradient(
+                                        listOf(
+                                            Color(0xFFf36b21),
+                                            Color(0xFFf9a521)
+                                        )
+                                    )
+                                )
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_moonbg),
+                                contentDescription = "moon",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                                    .graphicsLayer {
+                                        translationY = moonOffset
+                                    }
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_midbg),
+                                contentDescription = "mid bg",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                                    .graphicsLayer {
+                                        translationY = midBgOffset
+                                    }
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_outerbg),
+                                contentDescription = "outer bg",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                            )
+                        }
+                    }
+                    items(20) {
+                        Text(
+                            text = "Sample item",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
-}
 
-@Composable
-private fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") {
-            HomeScreen()
-        }
-        composable("chat") {
-            ChatScreen()
-        }
-        composable("settings") {
-            SettingsScreen()
-        }
-
+    private fun Float.toDp(): Dp {
+        return (this / Resources.getSystem().displayMetrics.density).dp
     }
+
 }
 
-@Composable
-private fun BottomNavigationBar(
-    items: List<BottomNavItem>,
-    navController: NavController,
-    modifier: Modifier = Modifier,
-    onItemClick: (BottomNavItem) -> Unit
-) {
-
-    val backStackEntry = navController.currentBackStackEntryAsState()
-
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = Color.DarkGray,
-        elevation = 5.dp
-    ) {
-        items.forEach { item ->
-            val selected = item.route == backStackEntry.value?.destination?.route
-            BottomNavigationItem(
-                selected = selected,
-                onClick = { onItemClick(item) },
-                selectedContentColor = Color.Green,
-                unselectedContentColor = Color.Gray,
-                icon = {
-                    Column(horizontalAlignment = CenterHorizontally) {
-                        if (item.badgeCount > 0) {
-                            BadgedBox(
-                                badge = { Badge { Text(text = item.badgeCount.toString()) } }) {
-                                Icon(
-                                    imageVector = item.icon,
-                                    contentDescription = item.name
-                                )
-                            }
-                        } else {
-                            Icon(
-                                imageVector = item.icon,
-                                contentDescription = item.name
-                            )
-                        }
-                        if (selected) {
-                            Text(text = item.name, textAlign = TextAlign.Center, fontSize = 10.sp)
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-private fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Home Screen")
-    }
-}
-
-@Composable
-private fun ChatScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Chat Screen")
-    }
-}
-
-@Composable
-private fun SettingsScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Settings Screen")
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
