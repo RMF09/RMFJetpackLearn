ba0f7649474fa86a15c5c025d1c9ea93d99b65b5
RMF <reksamfitrananda@gmail.com>
2022-03-28 16:11:07 +0700

    permission handling

--- app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
+++ app/src/main/java/com/example/rmfjetpacklearn/MainActivity.kt
@@ -1,61 +1,98 @@
 package com.example.rmfjetpacklearn
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.rmfjetpacklearn.ui.theme.ListItem
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.example.rmfjetpacklearn.ui.theme.RMFJetpackLearnTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionStatus
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
 
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
             RMFJetpackLearnTheme {
-                var items by remember {
-                    mutableStateOf(
-                        (1..20).map {
-                            ListItem(title = "Item $it")
-                        }
+                val permissionState = rememberMultiplePermissionsState(
+                    permissions = listOf(
+                        Manifest.permission.RECORD_AUDIO,
+                        Manifest.permission.CAMERA
                     )
+                )
+
+                val lifeCycleOwner = LocalLifecycleOwner.current
+                DisposableEffect(key1 = lifeCycleOwner) {
+                    val observer = LifecycleEventObserver { _, event ->
+                        if(event == Lifecycle.Event.ON_START){
+                            permissionState.launchMultiplePermissionRequest()
+                        }
+                    }
+                    lifeCycleOwner.lifecycle.addObserver(observer)
+
+                    onDispose {
+                        lifeCycleOwner.lifecycle.removeObserver(observer)
+                    }
                 }
-                LazyColumn(modifier = Modifier.fillMaxSize()) {
-                    items(items.size) { i ->
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    items = items.mapIndexed { j, item ->
-                                        if (i == j) {
-                                            item.copy(isSelected = !item.isSelected)
-                                        } else item
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    permissionState.permissions.forEach { perm ->
+                        when (perm.permission) {
+                            Manifest.permission.CAMERA -> {
+                                when (perm.status) {
+                                    is PermissionStatus.Granted -> {
+                                        Text(text = "Camera permission accepted")
+
+                                    }
+                                    is PermissionStatus.Denied -> {
+                                        val text =
+                                            if ((perm.status as PermissionStatus.Denied).shouldShowRationale)
+                                                "The camera is important for this app. Please grant the permission."
+                                            else
+                                                "Camera permission required for this feature to be available ." +
+                                                        "Please grant the permission"
+
+                                        Text(text = text)
+
                                     }
                                 }
-                                .padding(16.dp),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Text(text = items[i].title)
-                            if (items[i].isSelected) {
-                                Icon(
-                                    imageVector = Icons.Default.Check,
-                                    contentDescription = "check",
-                                    tint = Color.Red,
-                                    modifier = Modifier.size(20.dp)
-                                )
+                            }
+                            Manifest.permission.RECORD_AUDIO -> {
+                                when (perm.status) {
+                                    is PermissionStatus.Granted -> {
+                                        Text(text = "Record Audio permission accepted")
+
+                                    }
+                                    is PermissionStatus.Denied -> {
+                                        val text =
+                                            if ((perm.status as PermissionStatus.Denied).shouldShowRationale)
+                                                "The record audio is important for this app. Please grant the permission."
+                                            else
+                                                "Record audio permission required for this feature to be available. " +
+                                                        "Please grant the permission"
+
+                                        Text(text = text)
+
+                                    }
+                                }
                             }
                         }
                     }
